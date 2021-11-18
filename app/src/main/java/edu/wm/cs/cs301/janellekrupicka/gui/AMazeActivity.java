1bfef69f0e00970593134347b6ea2f6685adb84c
Janelle Krupicka <jekrupicka@email.wm.edu>
2021-11-17 20:19:52 -0500

    AMazeActivity goes to GeneratingActivity
    Selecting the explore button in AMazeActivity now takes you to GeneratingActivity. Back arrow in upper left corner of GeneratingActivity goes back to AMazeActivity.

--- app/src/main/java/edu/wm/cs/cs301/janellekrupicka/gui/AMazeActivity.java
+++ app/src/main/java/edu/wm/cs/cs301/janellekrupicka/gui/AMazeActivity.java
@@ -2,7 +2,9 @@ package edu.wm.cs.cs301.janellekrupicka.gui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.amazebyjanellekrupicka.R;
 
@@ -12,5 +14,9 @@ public class AMazeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_amaze);
+    }
+    public void exploreMaze(View view) {
+        Intent intent = new Intent(this, GeneratingActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/edu/wm/cs/cs301/janellekrupicka/gui/GeneratingActivity.java
@@ -0,0 +1,16 @@
+package edu.wm.cs.cs301.janellekrupicka.gui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.amazebyjanellekrupicka.R;
+
+public class GeneratingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_generating);
+    }
+}
