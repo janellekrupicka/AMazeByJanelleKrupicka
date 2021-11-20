c6cd77c324f1cefeec79b8f1e6a65bebc29f231d
Janelle Krupicka <jekrupicka@email.wm.edu>
2021-11-19 20:46:22 -0500

    Spinner in GeneratingActivity works correctly.
    Shows pop up if a driver hasn't been selected and moves to correct activity based on driver selected. Bug where PlayAnimationActivity sometimes doesn't show the Go2Winning. Bug where the pop up for "Select driver to continue" shows after moving to the playing activity.

--- app/src/main/java/edu/wm/cs/cs301/janellekrupicka/gui/GeneratingActivity.java
+++ app/src/main/java/edu/wm/cs/cs301/janellekrupicka/gui/GeneratingActivity.java
@@ -1,14 +1,21 @@
 package edu.wm.cs.cs301.janellekrupicka.gui;
 
+import static android.widget.Toast.LENGTH_LONG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ProgressBar;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.amazebyjanellekrupicka.R;
+import com.google.android.material.snackbar.Snackbar;
 
 public class GeneratingActivity extends AppCompatActivity {
 
@@ -23,6 +30,7 @@ public class GeneratingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_generating);
+
         progress = 0;
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
         progressBar.setMax(200);
@@ -39,14 +47,42 @@ public class GeneratingActivity extends AppCompatActivity {
                 }
                 handler.post(new Runnable() {
                     public void run() {
+                        Spinner play_type = findViewById(R.id.play_type);
+                        String selectedPlay = play_type.getItemAtPosition(play_type.getSelectedItemPosition()).toString();
+                        if(selectedPlay.equals("Manual")) {
+                            Toast.makeText(getBaseContext(), "Playing manually.", LENGTH_LONG).show();
+                            Intent intent = new Intent(getApplicationContext(), PlayManuallyActivity.class);
+                            startActivity(intent);
+                        }
+                        if (selectedPlay.equals("Wizard")) {
+                            Toast.makeText(getBaseContext(), "Playing with Wizard.", LENGTH_LONG).show();
+                            Intent intent = new Intent(getApplicationContext(), PlayAnimationActivity.class);
+                            startActivity(intent);
+                        }
+                        if (selectedPlay.equals("Wallfollower")) {
+                            Toast.makeText(getBaseContext(), "Playing with Wallfollower.", LENGTH_LONG).show();
+                            Intent intent = new Intent(getApplicationContext(), PlayAnimationActivity.class);
+                            startActivity(intent);
+                        }
+                        else {
+                            playTypeSpinnerSelection();
+                        }
                         // ---0 - VISIBLE; 4 - INVISIBLE; 8 - GONE---
-                        progressBar.setVisibility(View.GONE);
+                        //    progressBar.setVisibility(View.GONE);
+
+
+                        // String selectedPlay = play_type.getItemAtPosition(play_type.getSelectedItemPosition()).toString();
+                        // if(selectedPlay.equals("Manual")) {
+                        //   Intent intent = new Intent(getApplicationContext(), PlayManuallyActivity.class);
+                        // startActivity(intent);
+                        //}
+                        //if (selectedPlay.equals("Wizard") || selectedPlay.equals("Wallfollower")) {
+                        //  Intent intent = new Intent(getApplicationContext(), PlayAnimationActivity.class);
+                        //startActivity(intent);
+                        // }
                     }
                 });
-                Intent intent = new Intent(getApplicationContext(), PlayManuallyActivity.class);
-                startActivity(intent);
             }
-
             private int doSomeWork() {
                 try {
                     // ---simulate doing some work---
@@ -58,8 +94,46 @@ public class GeneratingActivity extends AppCompatActivity {
             }
         }).start();
     }
+    private void playTypeSpinnerSelection() {
+        // code from http://www.java2s.com/Code/Android/UI/SpinnerItemSelectedListener.htm
+        Spinner play_type = findViewById(R.id.play_type);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this, R.array.play_type_options,
+                android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        play_type.setAdapter(adapter);
+        play_type.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if (l == 0) {
+                    Toast.makeText(getBaseContext(), "Select driver to continue.", LENGTH_LONG).show();
+                }
+                if (l == 1) {
+                    Toast.makeText(getBaseContext(), "Playing manually.", LENGTH_LONG).show();
+                    Intent intent = new Intent(GeneratingActivity.this.getBaseContext(), PlayManuallyActivity.class);
+                    startActivity(intent);
+                }
+                if (l == 2) {
+                    Toast.makeText(getBaseContext(), "Playing with Wizard.", LENGTH_LONG).show();
+                    Intent intent = new Intent(GeneratingActivity.this.getBaseContext(), PlayAnimationActivity.class);
+                    startActivity(intent);
+                }
+                if (l == 3) {
+                    Toast.makeText(getBaseContext(), "Playing with Wallfollower.", LENGTH_LONG).show();
+                    Intent intent = new Intent(GeneratingActivity.this.getBaseContext(), PlayAnimationActivity.class);
+                    startActivity(intent);
+                }
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                Toast.makeText(getBaseContext(), "Nothing selected.", LENGTH_LONG).show();
+            }
+        });
+    }
     @Override
     public void onBackPressed() {
         finish();
     }
+
 }
+
